73843f4a97b32ac4f1f9eb762a8f031dd87c99e9
Bao Nguyen <baonguyena1@gmail.com>
2016-10-11 17:37:20 +0700

    Add choose avatar from camera or gallery

--- HarpaCrista/ProfileViewController.m
+++ HarpaCrista/ProfileViewController.m
@@ -15,7 +15,7 @@
 #import "CDUser+CoreDataProperties.h"
 #import "AppDelegate.h"
 
-@interface ProfileViewController () {
+@interface ProfileViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
     
     __weak IBOutlet UIImageView *_avatarImageView;
     __weak IBOutlet UILabel *_nameLabel;
@@ -23,6 +23,8 @@
     __weak IBOutlet UILabel *_bioLabel;
     __weak IBOutlet UILabel *_intrumentoLabel;
     __weak IBOutlet UILabel *_favouriesLabel;
+    __weak IBOutlet UIBarButtonItem *_editButton;
+    BOOL _canEdited;
 }
 
 @end
@@ -32,6 +34,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     INIT_INDICATOR;
+    _canEdited = YES;
     [self getUserInformation];
 }
 
@@ -44,7 +47,11 @@
     }
 }
 
+#pragma mark - Call Webservice
 - (void)getUserInformation {
+    if (![UserInfo shareInstance].isLogin) {
+        return;
+    }
     AFHTTPSessionManager *manager = [BaseApi HTTPSessionManagerRequiredAuthorization:YES];
     SHOW_INDICATOR(self.navigationController.view);
     [manager GET:@"http://harpacca.com/api/public/api/users/get_profile" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
@@ -58,6 +65,20 @@
                 HIDE_INDICATOR(YES);
             }
         });
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error: %@", error);
+        HIDE_INDICATOR(YES);
+    }];
+}
+
+- (void)updateUserInformation {
+    AFHTTPSessionManager *manager = [BaseApi HTTPSessionManagerRequiredAuthorization:YES];
+    SHOW_INDICATOR(self.navigationController.view);
+    [manager POST:@"http://harpacca.com/api/public/api/users/update_profile" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        
+    } progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        HIDE_INDICATOR(YES);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         NSLog(@"error: %@", error);
         HIDE_INDICATOR(YES);
@@ -114,4 +135,55 @@
     }
 }
 
+- (IBAction)didClickEditButton:(id)sender {
+    UIBarButtonItem *button = sender;
+    _canEdited = !_canEdited;
+    _canEdited ? [button setTitle:@"Edit"] : [button setTitle:@"Save"];
+    if (!_canEdited) {
+        //Update profile
+//        [self updateUserInformation];
+    }
+}
+
+- (IBAction)chooseAvatar:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"HarpaCrista" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *cameraAction = [UIAlertAction actionWithTitle:@"Take a New Photo"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+                                                             if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+                                                                 UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+                                                                 imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+                                                                 imagePickerController.allowsEditing = YES;
+                                                                 imagePickerController.delegate = self;
+                                                                 [self presentViewController:imagePickerController animated:YES completion:nil];
+                                                             }
+                                                         }];
+    [alert addAction:cameraAction];
+    
+    UIAlertAction *galleryAction = [UIAlertAction actionWithTitle:@"Select From Gallery" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        imagePickerController.allowsEditing = YES;
+        imagePickerController.delegate = self;
+        [self presentViewController:imagePickerController animated:YES completion:nil];
+    }];
+    [alert addAction:galleryAction];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+    [alert addAction:cancelAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark -  UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *image = [info valueForKey:UIImagePickerControllerEditedImage];
+    if (image == nil) {
+        image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    }
+    _avatarImageView.image = image;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 @end
